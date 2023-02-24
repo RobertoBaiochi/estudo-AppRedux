38aa9a7be6c8dad0c22f2f9c5968fdf9979c9762
RobertoBaiochi <robertombbaiochi@gmail.com>
2023-02-24 15:47:45 -0300

    feat: private routes with login required

---
+++ src/routes/MyRoute.jsx
@@ -0,0 +1,25 @@
+import P from 'prop-types';
+import { Redirect, Route } from 'react-router-dom';
+
+export default function MyRoute({ component: Component, isClosed, ...rest }) {
+  const isLoggedIn = false;
+
+  if (isClosed && !isLoggedIn) {
+    return (
+      <Redirect
+        to={{ pathname: '/login', state: { prevPath: rest.location.pathname } }}
+      />
+    );
+  }
+
+  return <Route {...rest} component={Component} />;
+}
+
+MyRoute.defaultProps = {
+  isClosed: false,
+};
+
+MyRoute.propTypes = {
+  component: P.oneOfType([P.element, P.func]).isRequired,
+  isClosed: P.bool,
+};
