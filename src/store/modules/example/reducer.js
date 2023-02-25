3afb217df111db88eb85a706c22b626af6c8d195
RobertoBaiochi <robertombbaiochi@gmail.com>
2023-02-24 22:52:54 -0300

    feat: add middleware with redux-saga

--- src/store/modules/example/reducer.js
+++ src/store/modules/example/reducer.js
@@ -1,13 +1,29 @@
+import * as types from '../types';
+
 const initialState = {
   botaoClicado: false,
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case 'BOTAO_CLICADO': {
+    case types.BOTAO_CLICADO_SUCCESS: {
+      // eslint-disable-next-line no-console
+      console.log('Sucesso');
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
+    }
+
+    case types.BOTAO_CLICADO_FAILURE: {
+      // eslint-disable-next-line no-console
+      console.log('Deu erro');
+      return state;
+    }
+
+    case types.BOTAO_CLICADO_REQUEST: {
+      // eslint-disable-next-line no-console
+      console.log('estou fazendo a requisição');
+      return state;
     }
 
     default:
