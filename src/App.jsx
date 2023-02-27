70e5cd771d7f88b1218c1ba445eefe9d9eca2fa1
RobertoBaiochi <robertombbaiochi@gmail.com>
2023-02-27 19:58:32 -0300

    feat: add redux-persist for localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 import Routes from './routes';
 import { ToastContainer } from 'react-toastify';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { Provider } from 'react-redux';
-import store from './store';
+import store, { persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Header />
-      <Routes />
-      <GlobalStyles />
-      <ToastContainer autoClose={3000} className="toast-container" />
+      <PersistGate persistor={persistor}>
+        <Header />
+        <Routes />
+        <GlobalStyles />
+        <ToastContainer autoClose={3000} className="toast-container" />
+      </PersistGate>
     </Provider>
   );
 }
