92e053c9a98783750ae47c6902f323e95f499d53
RobertoBaiochi <robertombbaiochi@gmail.com>
2023-03-02 19:22:15 -0300

    feat: add logout button and view login status

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,28 @@
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import {
+  FaCircle,
+  FaHome,
+  FaPowerOff,
+  FaSignInAlt,
+  FaUserAlt,
+} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import history from '../../services/history';
+
+import * as actions from '../../store/modules/auth/actions';
 
 import { Nav } from './styles';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+    history.push('/');
+  };
+
   return (
     <Nav>
       <Link to="/">
@@ -12,9 +31,18 @@ export default function Header() {
       <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to="/login">
-        <FaSignInAlt size={24} />
-      </Link>
+
+      {isLoggedIn ? (
+        <Link onClick={handleLogout} to="/logout" title="logout">
+          <FaPowerOff size={24} />
+        </Link>
+      ) : (
+        <Link to="/login">
+          <FaSignInAlt size={24} />
+        </Link>
+      )}
+
+      {isLoggedIn && <FaCircle size={24} color="#6f3" />}
     </Nav>
   );
 }
